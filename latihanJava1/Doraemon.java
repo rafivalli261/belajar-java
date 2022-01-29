24b6592ac9ca4137ae5ba2c1ceb006b8535ebb10
rafivalli261 <valliansyah@gmail.com>
2022-01-29 13:44:37 +0700

    belajar java

---
+++ latihanJava1/Doraemon.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+public class Doraemon {
+    public static void main(String[] args) {
+        // membuat objek array list
+        ArrayList kantongAjaib = new ArrayList();
+
+        // Mengisi kantong ajaib dengan 5 benda
+        kantongAjaib.add("Senter Pembesar");
+        kantongAjaib.add(532);
+        kantongAjaib.add("tikus");
+        kantongAjaib.add(1231234.132);
+        kantongAjaib.add(true);
+
+        // menghapus tikus dari kantong ajaib
+        kantongAjaib.remove("tikus");
+
+        // menampilkan isi kantong ajaib
+        System.out.println(kantongAjaib);
+
+        // menampilkan banyak isi kantong ajaib
+        System.out.println("Kantong ajaib berisi " + kantongAjaib.size() + " item");
+        
+    }
+}
---
+++ latihanJava1/RuangKelas.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class RuangKelas {
+    public static void main(String[] args) {
+        
+        // Membuat Array dan Scanner
+        String[][] meja = new String[2][3];
+
+        try(
+            Scanner scan = new Scanner(System.in);
+        ){
+            for(int bar = 0; bar < meja.length; bar++){
+                for(int kol = 0; kol < meja[bar].length; kol++){
+                    System.out.format("Siapa yang akan duduk di meja (%d, %d) : ",bar, kol);
+                    meja[bar][kol] = scan.nextLine();
+                }
+            }
+        }
+
+        // menampilkan isi array
+        System.out.println("-----------------------------");
+        for(int bar = 0; bar < meja.length; bar++){
+            for(int kol = 0; kol < meja[bar].length; kol++){
+                System.out.format("| %s | \t", meja[bar][kol]);
+            }
+            System.out.println("");
+        }
+        System.out.println("------------------------------");
+    }
+}
