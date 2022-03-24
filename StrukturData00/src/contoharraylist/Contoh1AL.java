d0e2c21b827621e4e916e6ece18e531a84999ef0
rafivalli261 <valliansyah@gmail.com>
2022-03-24 22:12:21 +0700

    belajar java - arraylist 1

---
+++ StrukturData00/src/contoharraylist/Contoh1AL.java
@@ -0,0 +1,17 @@
+package contoharraylist;
+
+import java.util.ArrayList;
+
+public class Contoh1AL {
+
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+
+        cars.add("Lamborgini");
+        cars.add("Luigi");
+        cars.add("Jerman");
+
+        System.out.println(cars);
+
+    }
+}
