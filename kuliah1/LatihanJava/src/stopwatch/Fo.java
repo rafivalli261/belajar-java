2630b23d99d6fb7c6eaa91007c4468f903910720
rafivalli261 <valliansyah@gmail.com>
2022-04-07 22:47:20 +0700

    membuat program penghitung waktu .. lagi

--- kuliah1/LatihanJava/src/stopwatch/Fo.java
+++ kuliah1/LatihanJava/src/stopwatch/Fo.java
@@ -1,31 +1,86 @@
 
 package stopwatch;
-
+import java.util.Scanner;
 /**
  *
  * @author ASUS
  */
 public class Fo {
-    private Object contents;
-    protected Object getContents(){
-        return contents;
+    
+    private final long nanoSecondsPerSecond = 1000000000;
+    private final long nanoSecondsPerMinute = 60000000000L;
+    private final long nanoSecondsPerHour = 3600000000000L;
+    
+    private long startTime = 0;
+    private long endTime = 0;
+    // private boolean isWaktuRun = false;
+    private boolean isMenu = true;
+    
+    Scanner ky = new Scanner(System.in);
+    
+    private void start(){
+        // this.isWaktuRun = true;
+        this.startTime = System.nanoTime();
+        System.out.println("Waktu tengah berjalan");
     }
-    protected void setContents(Object contents){
-        this.contents = contents;
+    
+    private void stop(){
+        // this.isWaktuRun = false;
+        this.endTime = System.nanoTime();
+        System.out.println("Waktu yang diperoleh adalah " + this.getJam() + " jam " + this.getMenit() + " menit " + this.getDetik() + " detik");
+        
     }
-    public void showPresent(){
-        System.out.println("Your gift : " + contents);
+    
+    private long getJam(){
+        long time = (this.endTime - this.startTime) / nanoSecondsPerHour ;
+        return time;
     }
+    private long getMenit(){
+        long time = ((this.endTime - this.startTime) % nanoSecondsPerHour) / nanoSecondsPerMinute ;
+        return time;
+    }
+    private long getDetik(){
+        long time = ((this.endTime - this.startTime) % nanoSecondsPerMinute) / nanoSecondsPerSecond ;
+        return time;
+    }
+    
+    private void eksekusiPilihan(int pilih){
+        switch(pilih){
+            case 1:
+                this.start();
+                break;
+            case 2:
+                this.stop();
+                break;
+            case 3:
+                this.isMenu = false;
+                break;
+            default:
+                System.out.println("Pilihan tidak ada, mungkin sedang ke lain hati");
+        }
+    }
+    
+    public void menu(){
+        while(this.isMenu){
+            System.out.println("******");
+            System.out.println("[1] Mulai");
+            System.out.println("[2] Berhenti");
+            System.out.println("[3] Keluar");
+            System.out.print("Pilih > ");
+            this.eksekusiPilihan(ky.nextInt());
+        }
+    }
+    
     
     
     public static void main(String[] args) {
-        Fo f = new Fo();
-        f.setContents(f);
-        f.showPresent();
-        String title = "Weather";
-        //int hot, double cold;
-        //System.out.println(hot + " " + title);
-        //        int Integer = 0;
-        float f2 = 5.0;
+        // membuat program penghitung waktu biasa
+        Fo programUtama = new Fo();
+        programUtama.menu();
     }
 }
+
+// masalah
+ // kurang exception
+// kurang banyak fungsinya
+// sebenarnya kurang interaktif
