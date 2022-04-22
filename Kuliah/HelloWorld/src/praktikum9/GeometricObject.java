0f63d9e81c2763d6391c8f9782cbf0772e66d161
rafivalli261 <valliansyah@gmail.com>
2022-04-22 22:24:20 +0700

    proses mengerjakan praktikum 9

---
+++ Kuliah/HelloWorld/src/praktikum9/GeometricObject.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum9;
+
+/**
+ *
+ * @author ASUS
+ */
+public class GeometricObject {
+    protected String warna;
+    protected boolean isi;
+    
+    public void getPerimeter(){
+        
+    };
+    public void getArea(){
+        
+    };
+}
