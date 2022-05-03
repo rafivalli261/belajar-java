34ce40e2609dac835be06e00795868bbf6eb8d5a
rafivalli261 <valliansyah@gmail.com>
2022-05-03 20:44:50 +0700

    menambah program password

--- kuliah1/LatihanJava/src/passgen/cobaPassSendiri.java
+++ kuliah1/LatihanJava/src/passgen/cobaPassSendiri.java
@@ -10,7 +10,7 @@ public class cobaPassSendiri {
          */
         String[] koleksi = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "="};
         // 16 + 26 + 26 +10 = 78
-        String[] pw = new String[8];
+        String[] pw = new String[12];
 
         for (int i = 0; i < pw.length; i++) {
             pw[i] = koleksi[(int) Math.floor(Math.random() * 100) % 78];
