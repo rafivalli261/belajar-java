4a1c97d68f4b4f283697cf4236d7563f3148f164
rafivalli261 <valliansyah@gmail.com>
2022-05-04 18:49:19 +0700

    belajar java fx part 3

---
+++ Kuliah/HelloWorld/src/gui_liang/NodeStyleRotateDemo.java
@@ -0,0 +1,32 @@
+package gui_liang;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.layout.StackPane;
+
+public class NodeStyleRotateDemo extends Application {
+
+    @Override // Override the start method in the application class
+    public void start(Stage primaryStage) {
+        // Create a scene and place a button in the application class
+        StackPane pane = new StackPane();
+        Button btOK = new Button("OK");
+        btOK.setStyle("-fx-border-color : blue;");
+        pane.getChildren().add(btOK);
+
+        pane.setRotate(45);
+        pane.setStyle("-fx-border-color: red; -fx-background-color: lightgray;");
+
+        Scene scene = new Scene(pane, 200, 250);
+        primaryStage.setTitle("NodeStyleRotateDemo"); // Set the stage title
+        primaryStage.setScene(scene); // place the scene in the stage
+        primaryStage.show(); // Display the stage
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+}
